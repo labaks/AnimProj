7fd3c4085b0c23122f968102043b1b29ff5fd7b5
labaks <las93@bk.ru>
2016-11-10 19:10:00 +0200

    try 2

--- app/src/main/java/labaks/animproj/MainActivity.java
+++ app/src/main/java/labaks/animproj/MainActivity.java
@@ -1,28 +1,52 @@
 package labaks.animproj;
 
+import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
-    TextView textViewInfo;
-    GifView gifView;
+    ImageView logo;
+    AnimationDrawable logoDrawable;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        gifView = (GifView) findViewById(R.id.gif_view);
-        textViewInfo = (TextView) findViewById(R.id.textinfo);
 
-        String stringInfo = "";
-        stringInfo += "Duration: " + gifView.getMovieDuration() + "\n";
-        stringInfo += "W x H: " + gifView.getMovieWidth() + " x " + gifView.getMovieHeight() + "\n";
+        logo = (ImageView) findViewById(R.id.logo);
 
-        textViewInfo.setText(stringInfo);
+        logo.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                Animation anim = AnimationUtils.loadAnimation(
+                        getApplicationContext(), R.anim.new_anim);
+                logo.startAnimation(anim);
+            }
+        });
+
+
+    }
+
+    class Starter implements Runnable {
+        private final AnimationDrawable ad;
+
+        Starter(AnimationDrawable ad) {
+            this.ad = ad;
+        }
+
+        public void run() {
+            ad.start();
+        }
     }
 
 }
